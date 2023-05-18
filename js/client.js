614a18b2f3c143dbf003cd362c5b11e849aa627c
adityamavadiya <mavadiyaaditya@gmail.com>
2023-05-18 11:11:42 +0530

    UI Changes

--- js/client.js
+++ js/client.js
@@ -1,12 +1,17 @@
 const socket = io('http://localhost:8000');
 
+
+// get DOM elements in respective variables
 const form = document.getElementById('send-container');   // get form by id
 const messageInput = document.getElementById('messageInp');   // get message written in text box
 const messageContainer = document.querySelector(".container");  // get container where we have to send our message
 var audio = new Audio('audio.mp3')
 
-// const name1 = prompt("Enter your name ?");
+// get name from user
+const name1 = prompt("Enter your name ?");
 console.log("name", name1)
+
+// function to append message written by user to container
 const append = (message, position)  =>{
     const messageElement = document.createElement('div')
     messageElement.innerText = message;
@@ -15,20 +20,28 @@ const append = (message, position)  =>{
     messageContainer.append(messageElement)
     if(position == 'left') audio.play();
 }
+
+// let the server know when a new user joins
 socket.emit('new-user-joined', name1);
 
+// if a new user joins then receive their name from server
 socket.on('user-joined', data =>{
     append(`${name1} has joined the chat.`, 'right')
 })
 
+// if server sends a message, then recieve it
 socket.on('receive', data =>{
     append(`${data.name} : ${data.message}`, 'left')
 })
 
+
+// when an user leaves the chat, then append that info in container
 socket.on('left', name =>{
-    append(`${name.name} left the chat.`, 'left')
+    append(`${name.name} left the chat.`, 'right')
 })
 
+
+// when the form is submitted , then send the data to server
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
     const message = messageInput.value;
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -8,16 +8,22 @@ const io = require("socket.io")(8000)
 const users = {};
 
 io.on("connection", socket =>{
+
+    //when a new user joins a chat, then it is broadcasted to others
     socket.on('new-user-joined', name=>{
         console.log("New user ", name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name)
     });
     
+
+    //when a message is sent by someone, then it is broadcasted to others
     socket.on('send',message =>{
         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
     });
 
+
+    // notify to all when somenone leaves the chat
     socket.on('disconnect', message =>{
         socket.broadcast.emit('left', {message: message, name: users[socket.id]})
         delete users[socket.id]
